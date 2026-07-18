c7b3a20a1f688019a4c99c31ef17b6a1b14f601d
giovanne28 <giovanneh.araujo05@gmail.com>
2026-07-18 17:05:40 -0300

    configurando bot

--- botcria/bot.py
+++ botcria/bot.py
@@ -11,7 +11,7 @@ TOKEN = os.getenv("DISCORD_TOKEN")
 intents = discord.Intents.default()
 intents.message_content = True
 
-bot = commands.Bot(command_prefix="!", intents=intents)
+bot = commands.Bot(command_prefix="@", intents=intents)
 
 @bot.event
 async def on_ready():
@@ -20,5 +20,26 @@ async def on_ready():
 @bot.command()
 async def oi(ctx):
     await ctx.send("Olá! Estou funcionando 🤖")
+    await asyncio.sleep(2)
+    await ctx.send("O que deseja?")
+
+@bot.command()
+async def tocar(ctx):
+
+    if ctx.author.voice is None:
+        await ctx.send("Você precisa estar em call jumento")
+        return
+
+    # pega a call onde o usuária ta
+    call = ctx.author.voice.channel
+
+    # bot entra na call
+    voz = await call.connect()
+
+    sirius = "musica.py"
+
+voz.play(discord.FFmpegPCMAudio(sirius))
+
+    await ctx.send("Música tocando")
 
 bot.run(TOKEN)
